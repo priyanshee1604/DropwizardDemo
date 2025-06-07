8a4b928aa62660e20d19e7275b438102c888bfa8
priyanshee1604 <priyansheesisodiya1604@gmail.com>
2025-06-07 19:16:58 +0530

    Implemented: Initial setup.

---
+++ src/main/java/org/example/trueApplication.java
@@ -0,0 +1,31 @@
+package org.example;
+
+import io.dropwizard.core.Application;
+import io.dropwizard.core.setup.Bootstrap;
+import io.dropwizard.core.setup.Environment;
+
+public class trueApplication extends Application<trueConfiguration> {
+
+    public static void main(final String[] args) throws Exception {
+        new trueApplication().run(args);
+    }
+
+    @Override
+    public String getName() {
+        return "Drop wizard Demo";
+    }
+
+    @Override
+    public void initialize(final Bootstrap<trueConfiguration> bootstrap) {
+        // TODO: application initialization
+    }
+
+    @Override
+    public void run(final trueConfiguration configuration,
+                    final Environment environment) {
+        // TODO: implement application
+        System.out.println("Template: " + configuration.getTemplate());
+        System.out.println("Default Name: " + configuration.getDefaultName());
+    }
+
+}
---
+++ src/main/java/org/example/trueConfiguration.java
@@ -0,0 +1,33 @@
+package org.example;
+
+import io.dropwizard.core.Configuration;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotEmpty;
+
+public class trueConfiguration extends Configuration {
+    @NotEmpty
+    private String template;
+
+    @NotEmpty
+    private String defaultName = "Stranger";
+
+    @JsonProperty
+    public String getTemplate() {
+        return template;
+    }
+
+    @JsonProperty
+    public void setTemplate(String template) {
+        this.template = template;
+    }
+
+    @JsonProperty
+    public String getDefaultName() {
+        return defaultName;
+    }
+
+    @JsonProperty
+    public void setDefaultName(String defaultName) {
+        this.defaultName = defaultName;
+    }
+}
